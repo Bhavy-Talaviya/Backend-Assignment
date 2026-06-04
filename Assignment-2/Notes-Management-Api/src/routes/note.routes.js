7b842e6c8c1858b7d8ec21ca03a6df1f9215b657
Bhavy-Talaviya <bhavy.talaviya.cg@gmail.com>
2026-06-04 22:24:20 +0530

    Backend Assignment-2 is a Complete

--- Assignment-2/Notes-Management-Api/src/routes/note.routes.js
+++ Assignment-2/Notes-Management-Api/src/routes/note.routes.js
@@ -2,6 +2,18 @@ const router = require("express").Router();
 
 const noteController = require("../controllers/note.controller");
 
+/* BULK ROUTES FIRST */
+
+router.post("/bulk", noteController.createBulkNotes);
+
+router.delete("/bulk", noteController.deleteBulkNotes);
+
+/* CRUD */
+
 router.post("/", noteController.createNote);
+
+router.get("/", noteController.getAllNotes);
+
+router.get("/:id", noteController.getNoteById);
 
 module.exports = router;
